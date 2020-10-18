377d44e1f1b5cc92cb0e537bd590fbedf6aaccee
Paul Ignatenko <paul@ignatenko.us>
2020-10-17 23:17:47 -0400

    Fixes

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+CWD=$(pwd)
+
 mkdir -p "$HOME/.config"
-ln -s nvim "$HOME/.config/nvim"
-ln -s zsh "$HOME/.zsh"
-ln -s zsh/zshrc "zsh/.zshrc"
+ln -s $CWD/nvim "$HOME/.config/nvim"
+ln -s $CWD/zsh "$HOME/.zsh"
+ln -s $CWD/zsh/zshrc "$HOME/.zsh/.zshrc"
+ln -s $CWD/zsh/zshenv "$HOME/.zsh/.zshenv"
