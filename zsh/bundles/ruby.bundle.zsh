27ece885cd144357a8be4c325b3e78718123b5e9
Paul Ignatenko <paul@ignatenko.us>
2020-10-25 01:08:54 -0400

    Remove bundles

--- zsh/bundles/ruby.bundle.zsh
+++
@@ -1,8 +0,0 @@
-source /Users/pignatenko/.zsh/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gem/gem.plugin.zsh
-fpath+=( /Users/pignatenko/.zsh/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gem )
-source /Users/pignatenko/.zsh/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/rails/rails.plugin.zsh
-fpath+=( /Users/pignatenko/.zsh/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/rails )
-source /Users/pignatenko/.zsh/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/rake-fast/rake-fast.plugin.zsh
-fpath+=( /Users/pignatenko/.zsh/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/rake-fast )
-source /Users/pignatenko/.zsh/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/bundler/bundler.plugin.zsh
-fpath+=( /Users/pignatenko/.zsh/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/bundler )
