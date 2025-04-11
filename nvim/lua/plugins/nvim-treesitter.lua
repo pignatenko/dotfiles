1d0976b40dc6dd3c25bf19ef40113fa004fcba60
Paul Ignatenko <Paul.Ignatenko@mheducation.com>
2025-04-11 01:40:34 -0400

    Add treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,31 +1,64 @@
 -- Syntax parser!
 plugin = {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    require'nvim-treesitter.configs'.setup {
-      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	build = ":TSUpdate",
+	event = { "VeryLazy" },
+	lazy = vim.fn.argc(-1) == 0, -- load tree sitter early when opening a file from the command line
+	init = function(plugin)
+		-- Performance: add nvim-treesitter queries to the rtp and it's custom query predicates early
+		-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+		-- no longer trigger the **nvim-treesitter** module to be loaded in time.
+		-- Luckily, the only things that those plugins need are the custom queries, which we make available
+		-- during startup.
+		require("lazy.core.loader").add_to_rtp(plugin)
+		require("nvim-treesitter.query_predicates")
+	end,
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- A list of parser names, or "all" (the five listed parsers should always be installed)
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
 
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = true,
 
-      ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-      -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+			---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+			-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-      highlight = {
-        enable = true,
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
-    }
-  end
+			highlight = {
+				enable = true,
+				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+				-- Using this option may slow down your editor, and you may see some duplicate highlights.
+				-- Instead of true it can also be a list of languages
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<space>v",
+					node_incremental = "]n",
+					scope_incremental = "]N",
+					node_decremental = "[n",
+				},
+			},
+		})
+	end,
+	keys = {
+		{ "<leader>v", desc = "Begin Increment Selection", mode = "n" },
+		{ "]n", desc = "Increment Selection", mode = "x" },
+		{ "]s", desc = "Scope Increment Selection", mode = "x" },
+		{ "[n", desc = "Decrement Selection", mode = "x" },
+	},
 }
 
-return plugin;
+return plugin
